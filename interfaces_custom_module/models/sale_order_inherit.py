4fdcbd4f3d849f5240ca1c193006ce59fb32883b
satoshiGoto <satoshigotofpuna@gmail.com>
2025-02-11 00:40:28 -0300

    [ADD][interfaces_test]: interfaces_custom_module: - Se agregan nuevas funcionalidades

--- interfaces_custom_module/models/sale_order_inherit.py
+++ interfaces_custom_module/models/sale_order_inherit.py
@@ -12,7 +12,7 @@ class interfaces_custom_module_sale_order_inheritance(models.Model):
     #Método encargado de verificar que el monto de la venta no sobrepase el límite de venta
     def action_confirm(self):
         for rec in self:
-            if rec.amount_total > rec.sale_limit:
+            if rec.amount_total > (rec.sale_limit*1.2):
                 raise UserError(f"El monto límite de venta es de {rec.sale_limit:,.2f} {rec.order_line.currency_id.symbol}")
 
         result = super(interfaces_custom_module_sale_order_inheritance, self).action_confirm()
--- interfaces_custom_module/models/sale_order_line_inherit.py
+++ interfaces_custom_module/models/sale_order_line_inherit.py
@@ -7,6 +7,7 @@ class interfaces_custom_module_sale_order_line_inheritance(models.Model):
     _inherit = 'sale.order.line'
 
     bar_code = fields.Char(string="Código de barra", related='product_id.barcode')
+    reference = fields.Char(string="Referencia Interna", related="product_id.default_code")
 
 
 
